bc29daf2e9b7ba46829202361a12147d23e88c79
papercutdesign <hi@papercutdesign.co>
2026-03-08 03:33:13 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS portfolio_items (
     id TEXT PRIMARY KEY,
     category TEXT NOT NULL,
     r2_key TEXT NOT NULL,
+    sort_order INTEGER DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
